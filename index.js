0f2f6d95523c05055da351c87f98bc9ce0689a29
sucaisheng <1206468307@qq.com>
2019-09-25 22:53:57 +0800

    管理者页面

---
+++ index.js
@@ -0,0 +1,19 @@
+
+new Vue({
+    el:"#Vue-app",
+    data:{
+    },
+    methods: {
+        denglu:function(){
+            var name = prompt("请输入账号：");
+            var password = prompt("请输入密码：");
+            // if(name=="15186478704"&&mima=="15186478704")
+            // {
+            //     alert("Welcom！");
+                
+            // }
+        }
+        
+    },
+
+});
